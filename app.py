07b8d666152e79e6b0b70cd70d51855ecd465190
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2024-06-12 19:54:38 -0300

    Execução do aplicativo

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
