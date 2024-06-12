44d1977b2a53c1d7939efebff1188ec01ccaee71
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2024-06-12 19:27:10 -0300

    Carregar o modelo e os rótulos

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ from keras.models import load_model
 from PIL import ImageOps
 
 app = Flask(__name__)
+
+# Carregar o modelo
+model = load_model("keras_model.h5", compile=False)
+# Carregar os rótulos
+class_names = open("labels.txt", "r").readlines()
