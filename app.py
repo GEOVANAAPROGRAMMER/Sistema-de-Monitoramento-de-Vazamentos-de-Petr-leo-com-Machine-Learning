35f56a015cf3a2b7ae570cfc8ebfcb1af5176996
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2024-06-12 19:53:21 -0300

    Rota para o feed de vídeo

--- app.py
+++ app.py
@@ -48,4 +48,8 @@ def gen_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
